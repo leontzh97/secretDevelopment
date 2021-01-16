ec9660ff2179b0f8c298f61663e99e83af0fbdf3
leon <leontzh97@hotmail.com>
2021-01-17 00:16:59 +0800

    Mysubscriptions update

--- MySubscriptions.js
+++ MySubscriptions.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   Alert,
   ScrollView,
@@ -10,7 +10,6 @@ import {
   Card,
   Icon,
 } from 'react-native-elements';
-
 import CardMedia from 'react-native-card-media';
 
 const path = 'https://raw.githubusercontent.com/dondoko-susumu/react-native-card-media-example/master/media/';
@@ -18,38 +17,86 @@ const files1 = [
   `${path}kids_play_640.jpeg`,
 ];
 
-export function MySubscriptions() {
-  return (
-    <View style={styles.scene}>
-      <ScrollView>
-        <View style={styles.container}>
-          <Card style={styles.cardStyle}>
-            <CardMedia
-              style={{ height: 200 }}
-              title="Title"
-              titleStyle={{ fontSize: 24, fontWeight: '400', lineHeight: 32, color: '#fafafa' }}
-              files={files1}
-              onPress={() => alert('Clicked')}
-            />
-            <Card.Divider>
-            <Text>Multiple image component for React Native</Text>
-            </Card.Divider>
-          </Card>
-        </View>
-      </ScrollView>
-    </View>
-  );
+const files2 = [
+  `${path}kids_play_640.jpeg`,
+  `${path}sea_kids_640.jpeg`,
+];
+
+const files3 = [
+  `${path}kids_play_640.jpeg`,
+  `${path}temple_640.jpeg`,
+  `${path}road_640.jpg`,
+];
+
+export class MySubscriptions extends Component<{}> {
+
+  onPress() {
+    Alert.alert('onPress');
+  }
+
+  render() {
+    return (
+      <View style={styles.scene}>
+        <ScrollView>
+          <View style={styles.container}>
+            <Card>
+              <CardMedia
+                style={{ height: 200 }}
+                title="Elementary Class"
+                titleStyle={{ fontSize: 24, fontWeight: '400', lineHeight: 32, color: '#fafafa' }}
+                files={files1}
+                onPress={() => this.onPress()}
+              />
+              <Card.Divider style={styles.cardStyle}>
+                <Text>
+                  For elementary players in drumming lessons
+                </Text>
+              </Card.Divider>
+            </Card>
+            <Card>
+              <CardMedia
+                style={{ height: 200 }}
+                title="Intermediate Class"
+                titleStyle={{ fontSize: 24, fontWeight: '400', lineHeight: 32, color: '#fafafa' }}
+                files={files2}
+                onPress={() => this.onPress()}
+              />
+              <Card.Divider style={styles.cardStyle}>
+                <Text>
+                  For intermediate players in drumming lessons
+                </Text>
+              </Card.Divider>
+            </Card>
+            <Card>
+              <CardMedia
+                style={{ height: 200 }}
+                title="Advance Class"
+                titleStyle={{ fontSize: 24, fontWeight: '400', lineHeight: 32, color: '#fafafa' }}
+                files={files3}
+                onPress={() => this.onPress()}
+              />
+              <Card.Divider style={styles.cardStyle}>
+                <Text>
+                  For advance players in drumming lessons
+                </Text>
+              </Card.Divider>
+            </Card>
+          </View>
+        </ScrollView>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   scene: {
-    marginTop: 20,
+    marginTop: 30,
   },
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#fff',
   },
   cardStyle: {
     width: 320,
